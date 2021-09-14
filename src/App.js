1930c665340d0111e4ec905c97ad128e9537a37d
JoaquinWeiss <joaco.w93@gmail.com>
2021-09-14 10:45:42 -0300

    changed dot in name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Joaquín Weiss.
+          Joaquín Weiss
         </p>
         <a
           className="App-link"
