6061687a8d72853bba2889dfe40e232542bf6545
JoaquinWeiss <joaco.w93@gmail.com>
2021-10-26 12:03:03 -0300

    worked on header carousel

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import logo from '../logo.svg';
+import { Carousel } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Joaquín Weiss
-                </p>
-        </header>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://s3.amazonaws.com/images-cdn.libooks.com/large_banner_misterio_4491b26a46.png"
+                        alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://s3.amazonaws.com/images-cdn.libooks.com/large_LBS_004_Ahora3y6_v1_desktop_8d05b503f2.jpg"
+                        alt="Second slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+            </header>
         </div>
     )
 }
